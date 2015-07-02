2aa0432be08baa46e66bf57a42b7066c80dd16ca
seairy <seairy@gmail.com>
2015-07-02 14:30:00 +0800

    init commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,4 @@
 # -*- encoding : utf-8 -*-
-# config valid only for current version of Capistrano
 lock '3.3.5'
 
 set :application, 'booking'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
 # -*- encoding : utf-8 -*-
-Administrator.create!(account: 'admin', password: '123456', password_confirmation: '123456', name: '王皓', available: true)
+Administrator.create!(account: 'admin', password: '123456', password_confirmation: '123456', name: '管理员', available: true)
