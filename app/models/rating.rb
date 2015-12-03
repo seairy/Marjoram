69cbba158c62c3be53668782593e2689a6c41aa3
seairy <seairy@gmail.com>
2015-12-03 13:55:04 +0800

    init commit

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -3,4 +3,12 @@ class Rating < ActiveRecord::Base
   belongs_to :expert
   scope :unhandled, -> { where('score IS NULL') }
   scope :handled, -> { where('score IS NOT NULL') }
+
+  def human_result
+    if self.score.blank?
+      '未评审'
+    else
+      "#{self.score}分"
+    end
+  end
 end
